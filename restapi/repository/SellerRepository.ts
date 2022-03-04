85ba297d97a4d74b406c8fad113b7dc5a302fd7a
Kuzko02801 <joasabm@gmail.com>
2022-03-04 17:24:35 +0100

    Fixed a problem in the repository caused by an "async" missing.

--- restapi/repository/SellerRepository.ts
+++ restapi/repository/SellerRepository.ts
@@ -3,19 +3,19 @@ import {ObjectId} from "mongodb"
 
 // Basic CRUD functions of seller.
 exports.sellerRest = {
-    all: function findAllSellers() {
+    all: async function findAllSellers() {
         return Seller.find({});
     },
-    findById: function findSellerById(id: ObjectId) {
+    findById: async function findSellerById(id: ObjectId) {
         return Seller.findById(id);
     },
-    update: function updateSeller(id: ObjectId, seller: Document){
+    update: async function updateSeller(id: ObjectId, seller: Document){
         return Seller.findByIdAndUpdate(id, seller);
     },
-    create: function createSeller(seller: Document) {
+    create: async function createSeller(seller: Document) {
         return Seller.create(seller);
     },
-    delete: function deleteSeller(id: ObjectId){
+    delete: async function deleteSeller(id: ObjectId){
         return Seller.findByIdAndDelete(id);
     }
 }
