3a01f926bf6609724481cfa9939bf6b443145a14
Rosa <uo277921@uniovi.es>
2022-03-14 22:54:16 +0100

    bug fixed :)

--- webapp/src/helpers/getAddress.ts
+++ webapp/src/helpers/getAddress.ts
@@ -10,7 +10,6 @@ const getAddress = async (webID: string) => {
     let profileDocumentURI = webID.split("#")[0]; // we are just interested in the card
     let myDataset = await getSolidDataset(profileDocumentURI); // obtain the dataset from the URI
     let profile = getThing(myDataset, webID); // we obtain the thing we are looking for from the dataset
-
     // we obtain the address
     return getStringNoLocale(profile as Thing, VCARD.street_address) as string;
 };
