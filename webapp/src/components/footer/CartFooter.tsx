d96ffbef78d3739a8b2139cdb955a253eacc7c90
Joaquín Britos Morales <joasabm@gmail.com>
2022-03-21 14:11:11 +0100

    The user is not allowed to checkout unless its logged in with its pod.

--- webapp/src/components/footer/CartFooter.tsx
+++ webapp/src/components/footer/CartFooter.tsx
@@ -15,7 +15,11 @@ const CartFooter = () => {
                 </div>
             </div>
             <div className="d-grid gap-2">
-                <Button className='btn btn-primary' onClick={() => {window.location.href="http://localhost:5000/solid/fetch/" + localStorage.getItem("webID")}}>Proceed with checkout</Button>
+                {(localStorage.getItem("loggedIn") == null) ?
+                <label>Por favor inicia sesion en tu pod.</label> :
+                    <Button className='btn btn-primary' onClick={() => {window.location.href="http://localhost:5000/solid/fetch/" + localStorage.getItem("webID")}}>Proceed with checkout</Button>
+                }
+
             </div>
         </div>
     )
