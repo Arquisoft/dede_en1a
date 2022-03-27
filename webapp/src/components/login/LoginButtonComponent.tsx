26f4a2be29d6ca018e45ec089df887b238bec071
uo277963 <uo277963@uniovi.es>
2022-03-27 15:21:19 +0200

    refactored localhost calls to include host parameter

--- webapp/src/components/login/LoginButtonComponent.tsx
+++ webapp/src/components/login/LoginButtonComponent.tsx
@@ -6,11 +6,12 @@ type Props = {
 }
 
 const LoginButton = (props: Props) => {
+    const apiEndPoint = process.env.REACT_APP_API_URI || "http://localhost:5000";
     const {setIsLoggedIn} = props
     return (
         <Button onClick={() => {
             setIsLoggedIn()
-            window.location.href="http://localhost:5000/solid/login"
+            window.location.href=apiEndPoint+"/solid/login"
         }}>
             Login
         </Button>
--- webapp/src/helpers/postData.ts
+++ webapp/src/helpers/postData.ts
@@ -1,8 +1,8 @@
 import {Order} from "../shared/shareddtypes";
 
 const postData = async (order: Order) => {
-
-    const fetchApi = await fetch('http://localhost:5000/order/add',{
+    const apiEndPoint = process.env.REACT_APP_API_URI || "http://localhost:5000";
+    const fetchApi = await fetch(apiEndPoint+'/order/add',{
         method: 'POST',
         body: JSON.stringify(order),
         headers:{
