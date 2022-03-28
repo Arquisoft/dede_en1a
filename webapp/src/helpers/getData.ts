886c7e96f36aa38c37495dbc635921cd45f15ea8
uo277963 <uo277963@uniovi.es>
2022-03-28 20:00:59 +0200

    re-reverting

--- webapp/src/helpers/getData.ts
+++ webapp/src/helpers/getData.ts
@@ -1,6 +1,8 @@
 const getProducts = async () => {
-    const apiEndPoint = "http://localhost:5000";
+    const apiEndPoint = process.env.REACT_APP_API_URI || "http://localhost:5000";
+    console.log(apiEndPoint);
     let response = await fetch(apiEndPoint + "/product/list");
+    console.log(response);
     return response.json();
 }
 
