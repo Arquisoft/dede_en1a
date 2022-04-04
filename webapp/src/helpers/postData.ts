b9d2da384da9c41fd6ef1fa66fbaa91d01288797
ludvij <vijande48@gmail.com>
2022-04-04 19:16:24 +0200

    fix

--- webapp/src/helpers/postData.ts
+++ webapp/src/helpers/postData.ts
@@ -23,15 +23,5 @@ const postData = async (order: OrderAdd) => {
     });
     return response;
 }
-// "webId" : "not a webId",
-// "address": "not an address",
-// "name" : "not a name",
-// "shippingPrice": 23,
-// "totalPrice": 23,
-// "products" : [
-// 	{"product" : "622fc1848aedec1b7f53677e", "number" : 2 },
-// 	{"product" : "6249cefb7fae0f9288454c36", "number" : 2 },
-// 	{"product" : "624b073c7f33d230eb264525", "number" : 2 }
-// ]
 
 export default postData;
