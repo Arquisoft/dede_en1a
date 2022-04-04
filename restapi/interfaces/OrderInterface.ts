65270a599688dc3746ce7c331afaa2e507545a0c
ludvij <vijande48@gmail.com>
2022-04-04 23:09:39 +0200

    renamed things

--- restapi/interfaces/OrderInterface.ts
+++ restapi/interfaces/OrderInterface.ts
@@ -8,7 +8,7 @@ export default interface IOrder extends Document {
     shippingPrice: number
     totalPrice: number
     products: [{
-		product_id : ObjectId, 
-		number : number
+		product : ObjectId, 
+		amount : number
 	}]
 }
