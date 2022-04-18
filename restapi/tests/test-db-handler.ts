c281476f6bc50f212943377cadfa4cb659bce633
uo277963 <uo277963@uniovi.es>
2022-04-18 16:50:40 +0200

    tests corregidos, proceso termina

---
+++ restapi/tests/test-db-handler.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import {MongoMemoryServer} from 'mongodb-memory-server';
+
+let mongod: MongoMemoryServer;
+
+/**
+ * Connect to the in-memory database.
+ */
+module.exports.connect = async () => {
+    mongod = await MongoMemoryServer.create();
+
+    const uri = mongod.getUri();
+    const mongooseOpts = {
+        useNewUrlParser: true,
+        autoReconnect: true,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 1000
+    };
+
+    await mongoose.connect(uri);
+}
+
+/**
+ * Drop database, close the connection and stop mongod.
+ */
+module.exports.closeDatabase = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+}
+
+/**
+ * Remove all the data for all db collections.
+ */
+module.exports.clearDatabase = async () => {
+    const collections = mongoose.connection.collections;
+
+    for (const key in collections) {
+        const collection = collections[key];
+        await collection.deleteMany({});
+    }
+}
