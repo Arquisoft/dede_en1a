e030101db89422f82eb036a4479ad64960d41003
landnn <landonnbrownn@gmail.com>
2022-04-23 18:39:10 +0200

    search by name
    barely works but it's something

--- webapp/src/components/listProducts/ListProducts.tsx
+++ webapp/src/components/listProducts/ListProducts.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 import {CartContext} from '../../context/CartContext';
 import useFetch from '../../hooks/useFetch';
 import {Product} from "../../shared/shareddtypes";
@@ -8,11 +8,23 @@ import LinearProgress from "@mui/material/LinearProgress";
 import Grid from '@mui/material/Grid';
 
 
+const getFilteredItems = (query: any, products: any) => {
+    console.log("Items are filtered");
+    if (!query){
+        return products;
+    }
+    return products.filter((product: any) => product.name.includes(query));
+}
+
 const ListProducts = () => {
 
     const {dispatch} = useContext(CartContext);
 
     const {products, isLoading} = useFetch();
+
+    const [query, setQuery] = useState("Screw");
+
+    const filteredItems = getFilteredItems(query, products);
 
     const handleAddToCart = (product: Product) => {
         dispatch({
@@ -21,7 +33,7 @@ const ListProducts = () => {
         });
     }
 
-    const renderCards = products.map(product => {
+    const renderCards = filteredItems.map((product: Product) => {
         return (
             <Grid key={product._id} item container xs={12} sm={6} md={4} lg={3}>
                 <CartProduct
@@ -36,6 +48,13 @@ const ListProducts = () => {
 
     return (
         <div>
+            <label>Search</label>
+            <input 
+                type="text" 
+                placeholder="Search..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)} />
+            
             <Grid container justifyContent="center" spacing={4}>
                 {renderCards}
             </Grid>
