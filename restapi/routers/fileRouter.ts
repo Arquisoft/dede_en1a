4870d2bd380ea5b21504d9d4e84ace881d6273bf
ludvij <vijande48@gmail.com>
2022-05-01 05:26:26 +0200

    image size

--- restapi/routers/fileRouter.ts
+++ restapi/routers/fileRouter.ts
@@ -17,7 +17,7 @@ const upload = multer({
 		cb (null, false)
 	}},
 	limits: {
-		fileSize: 10000000 // Sensitive: 10MB is more than the recommended limit of 8MB
+		fileSize: 8000000 // Sensitive: 10MB is more than the recommended limit of 8MB
 	}
 })
 
