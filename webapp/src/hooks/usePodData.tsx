aaf68a3bfa645804f22638057ca0f804bbc634b6
uo277963 <uo277963@uniovi.es>
2022-05-03 12:36:31 +0200

    process.env.REACT_APP_API_URI set

--- webapp/src/hooks/usePodData.tsx
+++ webapp/src/hooks/usePodData.tsx
@@ -14,7 +14,7 @@ export const usePodData = (session: SessionInfo["session"]) => {
 
     useEffect(() => {
         if(session.info.webId) {
-            axios.get((process.env.RESTAPI_URI || "http://localhost:5000") + "/solid/fetch/" + encrypt(session.info.webId)).then(
+            axios.get((process.env.REACT_APP_API_URI || "http://localhost:5000") + "/solid/fetch/" + encrypt(session.info.webId)).then(
                 response => {
                     setContactData({
                         fn: response.data.fn,
